efbbbe53c88b89a7c821659fb4823229433e0f79
Alan luu <luumanhquan.91@gmail.com>
2020-08-19 20:57:53 +0700

    feats: fix views

--- controllers/admin/home.go
+++ controllers/admin/home.go
@@ -1,12 +1,11 @@
 package admin
 
 import (
-	"log"
 	"net/http"
 )
 
 func HomeAdmin(w http.ResponseWriter, r *http.Request) {
-	log.Fatal(tmpl.ExecuteTemplate(w, "Admin-Home", nil))
+	tmpl.ExecuteTemplate(w, "Admin-Home", nil)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 }
